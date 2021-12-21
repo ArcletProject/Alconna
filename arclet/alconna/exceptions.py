e67ae20425e35c22e51f009d52088064408bfa3b
Tarrailt <42648639+RF-Tar-Railt@users.noreply.github.com>
2021-12-21 13:36:31 +0800

    0.3.3

--- arclet/alconna/exceptions.py
+++ arclet/alconna/exceptions.py
@@ -2,12 +2,8 @@ class ParamsUnmatched(Exception):
     """一个 text 没有被任何参数匹配成功"""
 
 
-class InvalidOptionName(Exception):
+class InvalidName(Exception):
     """option或subcommand的名字中填入了非法的字符"""
-
-
-class NullName(Exception):
-    """命令的名称写入了空字符"""
 
 
 class InvalidFormatMap(Exception):
