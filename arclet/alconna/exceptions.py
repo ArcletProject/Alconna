30e6ba2feff39f7b423858d1a4611b35c124de68
Tarrailt <42648639+RF-Tar-Railt@users.noreply.github.com>
2022-01-06 19:55:21 +0800

    0.4.2

--- arclet/alconna/exceptions.py
+++ arclet/alconna/exceptions.py
@@ -5,12 +5,8 @@ class ParamsUnmatched(Exception):
     """一个 text 没有被任何参数匹配成功"""
 
 
-class InvalidName(Exception):
-    """option或subcommand的名字中填入了非法的字符"""
-
-
-class InvalidFormatMap(Exception):
-    """错误的格式化参数串"""
+class InvalidParam(Exception):
+    """构造alconna时某个传入的参数不正确"""
 
 
 class NullTextMessage(Exception):
