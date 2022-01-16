99872c7abdc08ff42b20a82a08957d0d7f54b327
Tarrailt <42648639+RF-Tar-Railt@users.noreply.github.com>
2022-01-16 13:56:51 +0800

    0.5.4

--- arclet/alconna/component.py
+++ arclet/alconna/component.py
@@ -1,8 +1,6 @@
 """Alconna 的组件相关"""
 
-
 from typing import Union, Dict, List, Any, Optional, Callable, Type
-from .util import split_once
 from .types import NonTextElement
 from .base import TemplateCommand, Args
 
@@ -66,113 +64,72 @@ class Arpamar:
     """
 
     def __init__(self):
-        self.current_index: int = 0  # 记录解析时当前数据的index
-        self.is_str: bool = False  # 是否解析的是string
-        self.results: Dict[str, Any] = {'options': {}, 'main_args': {}}
-        self.raw_data: Dict[int, Union[List[str], NonTextElement]] = {}
-        self.need_main_args: bool = False
         self.matched: bool = False
         self.head_matched: bool = False
+        self.error_data: List[Union[str, NonTextElement]] = []
 
         self._options: Dict[str, Any] = {}
-        self._args: Dict[str, Any] = {}
+        self._other_args: Dict[str, Any] = {}
+        self._header: Optional[str] = None
+        self._main_args: Dict[str, Any] = {}
 
-    __slots__ = ("current_index", "is_str", "results", "raw_data", "need_main_args", "matched",
-                 "head_matched", "_options", "_args")
+    __slots__ = ("matched", "head_matched", "error_data", "_options", "_other_args", "_header", "_main_args")
 
     @property
     def main_args(self):
         """返回可能解析到的 main arguments"""
-        if self.need_main_args:
-            return self.results.get('main_args')
+        return self._main_args
 
     @property
     def header(self):
         """返回可能解析到的命令头中的信息"""
-        if 'header' in self.results:
-            return self.results['header']
+        if self._header:
+            return self._header
         return self.head_matched
 
     @property
     def all_matched_args(self):
         """返回 Alconna 中所有 Args 解析到的值"""
-        return {**self.results['main_args'], **self._args}
+        return {**self._main_args, **self._other_args}
 
     @property
     def option_args(self):
         """返回 Alconna 中所有 Option 里的 Args 解析到的值"""
-        return self._args
+        return self._other_args
 
-    def encapsulate_result(self) -> None:
+    def encapsulate_result(self, hdr: Optional[str], margs: Dict[str, Any], opts: Dict[str, Any]) -> None:
         """处理 Arpamar 中的数据"""
-        if not self.results.get('header'):
-            del self.results['header']
-        for k, v in self.results['options'].items():
+        self._header = hdr
+        self._main_args = margs['data']
+        for k, v in opts.items():
             self._options.setdefault(k, v)
             if isinstance(v, dict):
                 for kk, vv in v.items():
                     if not isinstance(vv, dict):
-                        self._args[kk] = vv
+                        self._other_args[kk] = vv
                     else:
-                        self._args.update(vv)
-        del self.results['options']
+                        self._other_args.update(vv)
 
     def get(self, name: Union[str, Type[NonTextElement]]) -> Union[Dict, str, NonTextElement]:
         """根据选项或者子命令的名字返回对应的数据"""
         if isinstance(name, str):
             if name in self._options:
                 return self._options[name]
-            if name in self._args:
-                return self._args[name]
+            if name in self._other_args:
+                return self._other_args[name]
         for _, v in self.all_matched_args:
             if isinstance(v, name):
                 return v
 
     def has(self, name: str) -> bool:
         """判断 Arpamar 是否有对应的选项/子命令的解析结果"""
-        return any([name in self._args, name in self._options])
+        return any([name in self._other_args, name in self._options])
 
     def __getitem__(self, item: Union[str, Type[NonTextElement]]):
         return self.get(item)
 
-    def next_data(self, separate: Optional[str] = None, pop: bool = True) -> Union[str, NonTextElement]:
-        """获取解析需要的下个数据"""
-        _text: str = ""  # 重置
-        _rest_text: str = ""
-
-        try:
-            if not self.raw_data[self.current_index]:
-                self.raw_data.pop(self.current_index)
-                self.current_index += 1
-
-            _current_data = self.raw_data[self.current_index]
-        except KeyError:
-            return ""
-
-        if isinstance(_current_data, list):
-            _text, _rest_text = split_once(_current_data[0], separate)
-            if not _rest_text and not pop:
-                return _current_data[0]
-            if not _rest_text and pop:
-                return _current_data.pop(0)
-            if _rest_text and pop:
-                self.raw_data[self.current_index][0] = _rest_text
-            return _text
-
-        if pop:
-            self.raw_data.pop(self.current_index)
-            self.current_index += 1
-        return _current_data
-
-    def reduce_data(self, data: Union[str, NonTextElement]):
-        try:
-            if isinstance(data, str) and isinstance(self.raw_data[self.current_index], list):
-                self.raw_data[self.current_index].insert(0, data)
-            else:
-                self.current_index -= 1
-                self.raw_data[self.current_index] = [data]
-        except KeyError:
-            pass
+    def __getattr__(self, item):
+        return self.get(item)
 
     def __repr__(self):
         attrs = ((s, getattr(self, s)) for s in self.__slots__)
