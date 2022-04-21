06ea2b98cddad8c65a27016bf5d2c9f52b67b3b0
RF-Tar-Railt <3165388245@qq.com>
2022-04-21 17:15:45 +0800

    🕶 0.8.3

--- graia/setup.py
+++ graia/setup.py
@@ -3,7 +3,7 @@ import setuptools
 setuptools.setup(
     name="arclet-alconna-graia",
     url="https://github.com/ArcletProject/Alconna/blob/master/arclet/alconna/graia",
-    version="0.0.8.1",
+    version="0.0.9",
     author="ArcletProject",
     author_email="rf_tar_railt@qq.com",
     description="Support Alconna to GraiaProject",
