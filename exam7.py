b33ca92dd11a740b977ebcf59729966d48ebdeec
RF-Tar-Railt <3165388245@qq.com>
2023-07-10 00:37:18 +0800

    :sparkles: enhance NodeMap

--- exam7.py
+++ exam7.py
@@ -4,21 +4,27 @@ from typing import Literal, overload, Optional
 from typing_extensions import Self
 from dataclasses import dataclass, field
 
-NodeMap = {}
-
-
 @dataclass
+class Scope:
+    refer: "Node"
+    substance: dict[str, "Node"] = field(default_factory=dict)
+
+NodeMap: dict[str, Scope] = {}
+
+
+@dataclass(eq=True, unsafe_hash=True)
 class Node:
     name: str
     args: Args = field(default_factory=Args)
     action: Action = field(default=store)
     help_text: str = field(default="unknown")
     dest: str = field(default="")
-    scope: str = field(init=False, default="$")
+    scope: str = field(default="$")
 
     def __post_init__(self):
         if not self.dest:
             self.dest = self.name.replace("-", "")
+        NodeMap.setdefault(self.path, Scope(self))
 
     __mapping__ = {
         ":args": "args",
@@ -26,6 +32,10 @@ class Node:
         ":help": "help_text",
         ":dest": "dest"
     }
+
+    @property
+    def path(self):
+        return f"{self.scope}.{self.name}" if self.scope != "$" else self.name
 
     @overload
     def assign(self, path: Literal[":args"], *, args: Args) -> Self:
@@ -86,21 +96,19 @@ class Node:
         if path.startswith(":"):
             raise AttributeError(f"Unknown attribute {path[1:]}")
         if spec:
-            query_path = f"{self.scope}.{self.name}.{path}" if self.scope != "$" else f"{self.name}.{path}"
+            query_path = f"{self.path}.{path}"
             if query_path not in NodeMap:
                 raise ValueError(f"Unknown node {query_path}")
-            setattr(NodeMap[query_path], self.__mapping__[spec], locals()[self.__mapping__[spec]])
+            setattr(NodeMap[query_path].refer, self.__mapping__[spec], locals()[self.__mapping__[spec]])
             return self
         parts = path.split(".")
         prev = self
         for part in parts[:-1]:
-            query_path = f"{prev.scope}.{prev.name}.{part}" if prev.scope != "$" else f"{prev.name}.{part}"
-            if query_path not in NodeMap:
+            if part not in NodeMap[prev.path].substance:
                 raise ValueError(f"Unknown node {part}")
-            prev = NodeMap[query_path]
-        new = Node(parts[-1], args, action, help_text, dest)
-        new.scope = f"{prev.scope}.{prev.name}" if prev.scope != "$" else prev.name
-        NodeMap[f"{new.scope}.{parts[-1]}"] = new
+            prev = NodeMap[prev.path].substance[part]
+        new = Node(parts[-1], args, action, help_text, dest, prev.path)
+        NodeMap[prev.path].substance[new.name] = new
         return self
 
     def select(self, path: str) -> "Node":
@@ -110,10 +118,9 @@ class Node:
             raise ValueError(f"Invalid path {path}")
         prev = self
         for part in path.split("."):
-            query_path = f"{prev.scope}.{prev.name}.{part}" if prev.scope != "$" else f"{prev.name}.{part}"
-            if query_path not in NodeMap:
-                raise ValueError(f"Unknown node {part}")
-            prev = NodeMap[query_path]
+            if part not in NodeMap[prev.path].substance:
+                raise ValueError(f"Unknown node {path}")
+            prev = NodeMap[prev.path].substance[part]
         return prev
 
 
@@ -127,3 +134,4 @@ print(node)
 print(foo)
 print(bar)
 print(NodeMap)
+assert node.select("foo.bar") == foo.select("bar")
