793ebf4b143d12bedf142c163916f9d417b96619
rf_tar_railt <3165388245@qq.com>
2024-04-04 23:24:45 +0800

    :bookmark: version 1.8.7

--- src/arclet/alconna/__init__.py
+++ src/arclet/alconna/__init__.py
@@ -50,7 +50,7 @@ from .typing import Nargs as Nargs
 from .typing import UnpackVar as UnpackVar
 from .typing import Up as Up
 
-__version__ = "1.8.6"
+__version__ = "1.8.7"
 
 # backward compatibility
 AnyOne = ANY
