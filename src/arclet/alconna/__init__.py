7aad613faac395fe2b50f5c48e1f4235a89a946d
RF-Tar-Railt <rf_tar_railt@qq.com>
2024-08-06 14:43:22 +0800

    :bookmark: version 1.8.22

--- src/arclet/alconna/__init__.py
+++ src/arclet/alconna/__init__.py
@@ -52,7 +52,7 @@ from .typing import UnpackVar as UnpackVar
 from .typing import Up as Up
 from .typing import StrMulti as StrMulti
 
-__version__ = "1.8.21"
+__version__ = "1.8.22"
 
 # backward compatibility
 AnyOne = ANY
