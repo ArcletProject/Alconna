4f0fd6f8f271e155bbc8f6fed1074fb0c23c8794
Elaina <GreyElaina@outlook.com>
2024-09-22 19:10:48 +0800

    sistana: remove duplicated util

--- src/arclet/alconna/sistana/utils/misc.py
+++ src/arclet/alconna/sistana/utils/misc.py
@@ -1,23 +1,7 @@
 from __future__ import annotations
 
 from dataclasses import dataclass
-from typing import TYPE_CHECKING, Generic, TypeVar, Union
-
-if TYPE_CHECKING:
-
-    def safe_extra_kw(
-        match_args=True,
-        kw_only=False,
-        slots=False,
-        weakref_slot=False,
-    ) -> dict[str, bool]: ...
-else:
-    from inspect import Signature
-
-    _available_dc_attrs = set(Signature.from_callable(dataclass).parameters.keys())
-
-    def safe_extra_kw(**kwargs):
-        return {k: v for k, v in kwargs.items() if k in _available_dc_attrs}
+from typing import Generic, TypeVar, Union
 
 
 T = TypeVar("T")
